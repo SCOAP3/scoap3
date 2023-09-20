54ad25cb61875273bed1a551d8d2da910f7f0328
ErnestaP <ernesta.petraityte@yahoo.com>
2023-09-20 16:14:08 +0200

    improvment

--- scoap3/articles/permissions.py
+++ scoap3/articles/permissions.py
@@ -3,9 +3,6 @@ from rest_framework import permissions
 
 class AdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
         if request.method == "POST" and not request.user.has_perm(
             "articles.add_article"
         ):
