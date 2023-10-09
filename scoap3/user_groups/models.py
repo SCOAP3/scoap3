87da60aa537936eaf457a91b0aa0360702afdcda
ErnestaP <ernesta.petraityte@yahoo.com>
2023-10-09 12:18:51 +0100

    pre-commit

--- scoap3/user_groups/models.py
+++ scoap3/user_groups/models.py
@@ -1,8 +1,9 @@
-from django.db import models
-from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.db import models
 
 User = get_user_model()
+
 
 class UserGroups(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -10,4 +11,3 @@ class UserGroups(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.group.name}"
-
