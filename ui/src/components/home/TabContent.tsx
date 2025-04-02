517d57c2c218879dd6d6fb1a62209d82cd89c34f
Lorenzo Vagliano <lorenzo.ventura.vagliano@cern.ch>
2025-04-02 17:44:52 +0200

    fix(UI): outdated partner countries
    Signed-off-by: vaglianolorenzo@gmail.com

--- ui/src/components/home/TabContent.tsx
+++ ui/src/components/home/TabContent.tsx
@@ -14,10 +14,11 @@ const TabContent: React.FC<TabContentProps> = ({ data, type }) => {
     <div className="tab-content">
       <ul>
         {data?.map((item: Journal | Country) => {
+          const displayKey = item?.key === "South Korea" ? "Korea, Republic of" : item?.key;
           return (
             <li key={item?.key} className="journal flex justify-between">
               <Link href={getSearchUrl({ [type]: item?.key }, true)}>
-                {item?.key}
+                {displayKey}
               </Link>
               <span className="badge">{item?.doc_count}</span>
             </li>
