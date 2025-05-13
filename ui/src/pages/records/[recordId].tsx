86165353151c980c80bff81ca9dba141f8152f71
Pamfilos Fokianos <pamfilosf@gmail.com>
2025-05-13 17:22:41 +0200

    global: ui - update for crawling and SEO
    Signed-off-by: Pamfilos Fokianos <pamfilosf@gmail.com>

--- ui/src/pages/records/[recordId].tsx
+++ ui/src/pages/records/[recordId].tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { GetServerSideProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from 'next/head';
 import { Text } from "react-mathjax2";
 
 import { Result } from "@/types";
@@ -12,68 +13,123 @@ import {
   renderComplexSytnax,
 } from "@/utils/utils";
 import { JsonPreview } from "@/components/shared/JsonPreview";
+import MathjaxContext from "@/components/shared/MathjaxContext";
+import TitleWithSupport from "@/components/search/TitleWithSupport";
 
 interface RecordPageProps {
   article: Result;
 }
 
-const RecordPage: React.FC<RecordPageProps> = ({ article }) => {
+const getMetaPubInfo = (article: Result) => {
+  if (article.publication_info.length > 0) {
+    return [
+      (article?.publication_info[0]?.page_start && article?.publication_info[0]?.page_end) ?
+        [
+          <meta key="page-start" name="citation_firstpage" content={`${article?.publication_info[0]?.page_start}`} />,
+          <meta key="page-end" name="citation_lastpage" content={`${article?.publication_info[0]?.page_end}`} />
+        ] : null,
+      <meta key="citation_publisher" name="citation_publisher" content={article.publication_info[0]?.publisher} />,
+      <meta key="citation_journal_title" name="citation_journal_title" content={article.publication_info[0]?.journal_title} />,
+      <meta key="citation_journal_volume" name="citation_journal_volume" content={article.publication_info[0]?.journal_volume} />,
+      <meta key="citation_journal_issue" name="citation_journal_issue" content={article.publication_info[0]?.journal_issue} />
+
+    ]
+  }
+}
+
+export default function RecordPage({ article }: RecordPageProps) {
   return (
-    <div className="container">
-      <div className="container-inner">
-        <div className="flex flex-col md:flex-row">
-          <div className="detail-page-main">
-            <Text
-              text={
-                <h2
-                  className="font-normal mb-3"
-                  dangerouslySetInnerHTML={{
-                    __html: renderComplexSytnax(cleanText(article?.title)),
-                  }}
+    <>
+      <Head >
+        <title>{article.title}</title>
+
+
+        <meta name="citation_abstract_html_url" content="{{ request.url }}" />
+
+        {article?.article_identifiers?.map((identifier, idx) => {
+          if (identifier?.identifier_type == "arXiv") {
+            return (
+              <meta key={idx} name="citation_technical_report_number" content={`${identifier?.identifier_value}`} />
+            )
+          }
+        })}
+
+        {article?.related_files?.map((file, idx) => {
+          if (file?.file.indexOf(".pdf") > 0) {
+            return (
+              <meta key={idx} name="citation_pdf_url" content={`${file?.file}`} />
+            )
+          }
+        })}
+
+        <meta name="description" content={article.abstract} />
+        <meta name="citation_title" content={article.title} />
+        {article?.authors.map((author, idx) => <meta key={idx} name="citation_author" content={`${author?.last_name}, ${author?.first_name}`} />)}
+        <meta name="citation_publication_date" content={article.publication_date.replace("-", "/")} />
+        {getMetaPubInfo(article)}
+        <meta name="citation_doi" content={article.doi} />
+      </Head>
+      <div className="container">
+        <div className="container-inner">
+          <div className="flex flex-col md:flex-row">
+            <MathjaxContext>
+              <div className="detail-page-main">
+                <TitleWithSupport title={article?.title} showExtra />
+                <Authors
+                  authors={article?.authors}
+                  page="detail"
+                  affiliations
+                  className="mb-3"
                 />
-              }
-            />
-            <Authors
-              authors={article?.authors}
-              page="detail"
-              affiliations
-              className="mb-3"
-            />
-            <Text
-              text={
-                <p
-                  className="text-justify leading-relaxed"
-                  dangerouslySetInnerHTML={{
-                    __html: renderComplexSytnax(cleanText(article?.abstract)),
-                  }}
+                <Text
+                  text={
+                    <p
+                      className="text-justify leading-relaxed"
+                      dangerouslySetInnerHTML={{
+                        __html: renderComplexSytnax(cleanText(article?.abstract)),
+                      }}
+                    />
+                  }
                 />
-              }
-            />
-            <JsonPreview article={article} />
-          </div>
-          <div className="detail-page-right">
-            <DetailPageInfo article={article} />
+                <JsonPreview article={article} />
+              </div>
+            </MathjaxContext>
+            <div className="detail-page-right">
+              <DetailPageInfo article={article} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    </>
+  )
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const recordId = context.query["recordId"];
-  const req = context?.req;
-  const clientIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const recordId = context.params?.recordId;
   const res = await fetch(getApiUrl() + `/${recordId}`, {
-    "headers": {
+    headers: {
       ...authToken?.headers,
-      "X-Forwarded-For": clientIp as string,
-    }
+    },
   });
+
+  if (!res.ok) {
+    return {
+      notFound: true,
+    };
+  }
+
   const article = (await res.json()) as Result;
 
-  return { props: { article } };
+  return {
+    props: { article },
+    revalidate: 6000,
+  };
 };
-
-export default RecordPage;
